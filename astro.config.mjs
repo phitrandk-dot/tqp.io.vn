bdf12e16195cbcf801c3c225f702c0440327f614
Tran_Phi <phitran.dk@gmail.com.com>
2025-09-04 15:37:07 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://phitrandk-dot.github.io/tqp.io.vn/',
+	site: 'https://tqp.io.vn',
 	integrations: [mdx(), sitemap()],
 });
